7edace9c6f2c601380d55f8e074c64967ec02a00
Yuuki_desktop <anni_yuuki@yahoo.co.jp>
2024-05-12 03:15:36 +0900

    trie_tree updates

--- trie_tree.cpp
+++ trie_tree.cpp
@@ -5,56 +5,65 @@ template<typename T>
 class trie_tree {
 public:
 
-	trie_tree() :dat(1, { T(),-1,{},0,0 }), u({ T(), -1, map<T, ll>{}, 0, 0 }) {}
+	struct node {
+		T data;
+		ll par;
+		map<T, ll> childs;
+		ll shares_count, end_count;
+	};
 
-	ll insert(vector<T> ar) {
-		get<3>(dat[0])++;
+	trie_tree() : dat(1, u) {}
+
+	ll insert(const vector<T>& ar) {
+		dat[0].shares_count++;
 		ll p = 0;
 		for (const T& v : ar) {
-			if (get<2>(dat[p]).contains(v)) {
-				p = get<2>(dat[p])[v];
+			if (dat[p].childs.contains(v)) {
+				p = dat[p].childs[v];
 			}
 			else {
-				get<2>(dat[p])[v] = dat.size();
+				dat[p].childs[v] = dat.size();
 				dat.push_back(u);
-				get<0>(dat.back()) = v;
-				get<1>(dat.back()) = p;
+				dat.back().data = v;
+				dat.back().par = p;
 				p = dat.size() - 1;
 			}
-			get<3>(dat[p])++;
+			dat[p].shares_count++;
 		}
-		get<4>(dat[p])++;
+		dat[p].end_count++;
 		return p;
 	}
 
-	ll count(vector<T> ar)const {
+	
+
+	ll count(const vector<T>& ar)const {
 		ll p = dive(ar);
-		return p != -1 ? get<4>(dat[p]) : 0;
+		return p != -1 ? dat[p].end_count : 0;
 	}
 
-	ll prefix(vector<T> ar)const {
+	ll prefix(const vector<T>& ar)const {
 		ll p = dive(ar);
-		return p != -1 ? get<3>(dat[p]) : 0;
+		return p != -1 ? dat[p].shares_count : 0;
 	}
 
-	void erase(vector<T> ar) {
+	void erase(const vector<T>& ar) {
 		if (count(ar)) {
 			ll p = dive(ar);
-			get<4>(dat[p])--;
+			dat[p].end_count--;
 			while (p != 0) {
-				get<3>(dat[p])--;
-				p = get<1>(dat[p]);
+				dat[p].shares_count--;
+				p = dat[p].par;
 			}
-			get<3>(dat[0])--;
+			dat[p].shares_count--;
 		}
 		return;
 	}
 
-	ll dive(vector<T> ar)const {
+	ll dive(const vector<T>& ar)const {
 		ll p = 0;
 		for (const T& v : ar) {
-			if (get<2>(dat[p]).contains(v)) {
-				p = get<2>(dat[p]).at(v);
+			if (dat[p].childs.contains(v)) {
+				p = dat[p].childs.at(v);
 			}
 			else {
 				return -1;
@@ -63,48 +72,53 @@ public:
 		return p;
 	}
 
-	ll size(void)const { return get<3>(dat[0]); }
+	ll size(void)const { return dat[0].shares_count; }
 
-	const vector<tuple<T, ll, map<T, ll>, ll, ll>>& getdat(void) { return dat; }
+	void clear(void) { dat.assign(1, u); }
+
+	const vector<node>& getdat(void) { return dat; }
 
 private:
 
-	vector<tuple<T, ll, map<T, ll>, ll, ll>> dat;	//dat[i] == { data, par, [ch] == chid, shares_count, end_count }
-	const tuple<T, ll, map<T, ll>, ll, ll> u;
+	vector<node> dat;
+	static const node u;
 };
+template<typename T>
+typename const trie_tree<T>::node trie_tree<T>::u{ T(), -1, map<T, ll>{}, 0, 0 };
 
 class trie_tree_string {
 public:
+	trie_tree_string() = default;
 
-	trie_tree_string() {};
-
-	ll insert(string s) {
+	ll insert(const string& s) {
 		return tr.insert(stove(s));	
 	}
 
-	ll count(string s)const {
+	ll count(const string& s)const {
 		return tr.count(stove(s));
 	}
 
-	ll prefix(string s)const {
+	ll prefix(const string& s)const {
 		return tr.prefix(stove(s));
 	}
 
-	void erase(string s) {
+	void erase(const string& s) {
 		tr.erase(stove(s));
 	}
 
-	ll dive(string s)const {
+	ll dive(const string& s)const {
 		return tr.dive(stove(s));
 	}
 
-	const vector<tuple<char, ll, map<char, ll>, ll, ll>>& getdat(void) { return tr.get(); }
+	const vector<trie_tree<char>::node>& getdat(void) { return tr.getdat(); }
 	
 	ll size(void)const { return tr.size(); }
 
+	void clear(void) { tr.clear(); }
+
 private:
 
-	static vector<char> stove(string s) {
+	static vector<char> stove(const string& s) {
 		return vector<char>(s.begin(), s.end());
 	}
 
